99f10b5b749a082fddff7fea23662cfb38a176ef
scare-destiny <zhenya.venger@gmail.com>
2024-01-30 20:40:02 +0200

    x

--- src/app/(components)/playground/text-generate-effect/page.tsx
+++ src/app/(components)/playground/text-generate-effect/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { motion } from 'framer-motion'
 import { cn } from '@/utils/cn'
 
-export const TextGenerateEffect = ({
+const TextGenerateEffect = ({
   words,
   className,
 }: {
