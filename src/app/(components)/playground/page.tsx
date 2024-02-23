cca96655533f30540a0028be952be84cdc2d4af1
scare-destiny <zhenya.venger@gmail.com>
2024-02-23 23:10:45 +0200

    x

--- src/app/(components)/playground/page.tsx
+++ src/app/(components)/playground/page.tsx
@@ -1,17 +1,30 @@
 import Link from 'next/link'
 
 import { getComponents } from '@/lib/components'
+import { Resources } from '@/components/Resources'
 
 export default async function Page(): Promise<JSX.Element> {
   const componentsData = await getComponents()
 
   return (
-    <div>
+    <div className=" grid grid-cols-1 gap-4 sm:grid-cols-2">
       {componentsData.map((component) => (
-        <div key={component}>
-          <Link href={`/playground/${component}`}>{component}</Link>
+        <div
+          key={component}
+          className="relative flex items-center space-x-3 rounded-lg border border-gray-300 bg-gray-300 px-6 py-5 shadow-sm focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:border-gray-400"
+        >
+          <div className="min-w-0 flex-1">
+            <Link
+              href={`/playground/${component}`}
+              className="focus:outline-none"
+            >
+              <span className="absolute inset-0" aria-hidden="true" />
+              <p className="text-sm font-medium text-gray-900">{component}</p>
+            </Link>
+          </div>
         </div>
       ))}
+      <Resources />
     </div>
   )
 }
